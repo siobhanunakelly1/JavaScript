d0b8c30b3b211ba5e7a9899fbfce1ecf021e56b0
siobhanunakelly1 <43471927+siobhanunakelly1@users.noreply.github.com>
2020-10-14 17:42:57 +0100

    Update

---
+++ js.js
@@ -0,0 +1,29 @@
+
+/*var example = "Variable"
+document.write(example);
+example = "Variable2"
+document.write(example);
+
+var visitorAge = prompt("what is your age");
+var visitorName = prompt("what is your name");
+
+document.write(visitorName + " age is " + visitorAge);
+
+var visitorAge = prompt("what is your age");
+var age = parseInt(visitorAge);
+document.write(age + 2);
+
+var ans = prompt("What is 2+2");
+if(ans === "4"){
+	document.write("correct");
+}else{
+	document.write("wrong");
+}*/
+
+function Function(){
+	document.write("correct");
+}
+
+Function();
+Function();
+Function();
